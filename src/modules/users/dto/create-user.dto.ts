ce09e4a335ba9de720b7f0c6e1a086a88fccbf6e
Adriano hilario <adrianoferreira31@gmail.com>
2022-09-08 11:09:32 -0300

    🚧  criando Dto

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -2,12 +2,44 @@ import { IsEmail, IsString, Matches, MinLength } from 'class-validator';
 export class CreateUserDto {
   id?: string;
 
+  @MinLength(3)
+  @Matches(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/, {
+    message: 'Name is only allowed Letters',
+  })
   @IsString()
-  name: string;
-  email: string;
-  password: string;
-  cpf: string;
-  doc_identity: string;
-  address: string;
-  cep: string;
+  readonly name: string;
+
+  @IsEmail()
+  readonly email: string;
+
+  @IsString()
+  @MinLength(8)
+  @Matches(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])(?:([0-9a-zA-Z$*&@#])(?!\1)){8,}$/,
+    {
+      message: 'Password does not meet the necessary requirements!',
+    },
+  )
+  readonly password: string;
+
+  @Matches(/^[0-9]*$/, {
+    message: 'CPF is only allowed Numbers',
+  })
+  @MinLength(11)
+  @IsString()
+  readonly cpf: string;
+
+  @Matches(/^[0-9]*$/, {
+    message: 'document is only allowed Numbers',
+  })
+  @IsString()
+  readonly doc_identity: string;
+
+  @MinLength(5)
+  @IsString()
+  readonly address: string;
+
+  @MinLength(8)
+  @IsString()
+  readonly cep: string;
 }
--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,4 +1,54 @@
 import { PartialType } from '@nestjs/mapped-types';
+import {
+  IsEmail,
+  IsOptional,
+  IsString,
+  Matches,
+  MinLength,
+} from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsString()
+  @MinLength(3)
+  @Matches(/^[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$/, {
+    message: 'Name is only allowed Letters',
+  })
+  @IsOptional()
+  readonly name: string;
+
+  @IsOptional()
+  @IsEmail()
+  readonly email: string;
+
+  @MinLength(8)
+  @Matches(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])(?:([0-9a-zA-Z$*&@#])(?!\1)){8,}$/,
+    {
+      message: 'Password does not meet the necessary requirements!',
+    },
+  )
+  @IsOptional()
+  readonly password: string;
+
+  @Matches(/^[0-9]*$/, {
+    message: 'CPF is only allowed Numbers',
+  })
+  @IsOptional()
+  @MinLength(11)
+  readonly cpf: string;
+
+  @IsOptional()
+  @Matches(/^[0-9]*$/, {
+    message: 'document is only allowed Numbers',
+  })
+  readonly doc_identity: string;
+
+  @IsOptional()
+  @MinLength(5)
+  readonly address: string;
+
+  @IsOptional()
+  @MinLength(8)
+  readonly cep: string;
+}

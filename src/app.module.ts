7f0a8f92873cf61d3c39f55140f84dad938bc4e9
Adriano hilario <adrianoferreira31@gmail.com>
2022-09-13 16:06:00 -0300

    🚧  importação do modulo account

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AccountsModule } from './modules/accounts/accounts.module';
-import { AccountsModule } from './modules/accounts/accounts.module';
 
 @Module({
   imports: [
--- src/modules/accounts/accounts.module.ts
+++ src/modules/accounts/accounts.module.ts
@@ -1,9 +1,11 @@
+import { PrismaService } from './../../database/prisma.service';
+import { AccountsRepository } from './account.repository';
 import { Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
 
 @Module({
   controllers: [AccountsController],
-  providers: [AccountsService],
+  providers: [AccountsService, AccountsRepository, PrismaService],
 })
 export class AccountsModule {}
